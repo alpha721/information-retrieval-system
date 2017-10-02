1145cd4f094fb494f76e1fa2aacdfc2812273e83
Neetu Yadav <yadavneet7@gmail.com>
2017-10-02 23:04:55 +0530

    deleted some unnecessary files

--- semanticpy-master/main.py
+++
@@ -1,9 +0,0 @@
-from semanticpy.vector_space import VectorSpace
-
-vector_space = VectorSpace(["The cat in the hat disabled", "A cat is a fine pet ponies.", "Dogs and cats make good pets.","I haven't got a hat."])
-
-#Search for cat
-print vector_space.search(["cat"])
-
-#Show score for relatedness against document 0
-print vector_space.related(0) 
--- semanticpy-master/tests/unit_tests/transform/lda_test.py
+++
@@ -1,6 +0,0 @@
-from unittest import TestCase
-from semanticpy.transform.lda import LDA
-
-class LDATest(TestCase):
-    def it_should_do_lsa_test(self):
-        pass
